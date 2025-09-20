950dfa85edbbdd6c21cb8a68ca9fd5b059f24342
Aleksandr Kravchenko <aleksandr.kravchenko@dxc.com>
2025-09-20 15:43:50 +0200

    - implemented basic functionality;

---
+++ ImgWarehouse/Core/ContactList.cs
@@ -0,0 +1,51 @@
+﻿using ImageMagick;
+using ImgWarehouse.Core.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace ImgWarehouse.Core
+{
+    internal class ContactList
+    {
+        private static readonly int COLUMNS = 6;
+        private static readonly int ROWS = 0;
+
+        private static readonly uint WIDTH = 200;
+        private static readonly uint HEIGHT = 200;
+
+        private static readonly int SPACING = 5;
+
+        private ILogger Logger { get; set; } = LoggerServiceLocator.CreateLogger<ContactList>();
+
+        public void Create(List<string> imagePaths, string outputFile)
+        {
+            Logger.LogInformation("Creating Contact List");
+
+            using (var images = new MagickImageCollection())
+            {
+                foreach (var path in imagePaths)
+                {
+                    var img = new MagickImage(path)
+                    {
+                        Label = Path.GetFileName(path)
+                    };
+                    img.Resize(WIDTH, HEIGHT);
+                    images.Add(img);
+                }
+
+                var settings = new MontageSettings
+                {
+                    Geometry = new MagickGeometry($"{WIDTH}x{HEIGHT}+{SPACING}+{SPACING}"),
+                    TileGeometry = new MagickGeometry($"{COLUMNS}x{(ROWS > 0 ? ROWS.ToString() : "")}"),
+                    //Label = true
+                };
+
+                using (var montage = images.Montage(settings))
+                {
+                    montage.Write(outputFile);
+                }
+            }
+
+            Logger.LogInformation($"Contact List {outputFile} created");
+        }
+    }
+}
---
+++ ImgWarehouse/Core/Logging/LoggerServiceLocator.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ImgWarehouse.Core.Logging;
+
+internal static class LoggerServiceLocator
+{
+    private static ILoggerFactory? _factory;
+    public static void Init(ILoggerFactory factory) => _factory = factory;
+    public static ILogger CreateLogger<T>() => _factory!.CreateLogger<T>();
+}
